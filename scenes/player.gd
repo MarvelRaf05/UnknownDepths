4dae3dc24bdd4f5f985b0387d653c209265ce3fe
Russell <rvandenh01@gmail.com>
2026-03-30 12:04:58 +0200

    Point and click movement. Need to add a rotation clamp

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,16 +1,26 @@
 extends CharacterBody2D
 
-const SPEED = 400.0
+@export var speed = 400
+var target = position
 
-func _physics_process(_delta):
-	var direction = Input.get_vector(
-		'ui_left', 'ui_right', 'ui_up', 'ui_down')
+func get_input():
+	look_at(get_global_mouse_position())
+	rotationClamp()
+	velocity = transform.x * Input.get_axis("ui_down", "ui_up") * speed
 
-	if direction.length():
-		velocity = direction * SPEED
-		#look_at(position + direction)
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.y = move_toward(velocity.y, 0, SPEED)
+func _input(event):
+	# Use is_action_pressed to only accept single taps as input instead of mouse drags.
+	if event.is_action_pressed(&"click"):
+		target = get_global_mouse_position()
+		look_at(target)
 
-	move_and_slide()
+func _physics_process(delta):
+	velocity = transform.x * speed	
+	if position.distance_to(target) > 10:
+		move_and_slide()
+
+func rotationClamp():
+	if rotation > 45 and rotation < 215:
+		rotation = 45
+	if rotation > 215:
+		scale.x * -1
